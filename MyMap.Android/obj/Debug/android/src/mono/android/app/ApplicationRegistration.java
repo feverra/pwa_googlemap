0907c836b2503aa98878bd664971606fd480ee1b
unknown <feverra@gmail.com>
2019-09-23 03:19:02 -0700

    first commit

---
+++ MyMap.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("MyMap.Droid.MainApplication, MyMap.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md521b7498ffef337055026c234c76b9faa.MainApplication.class, md521b7498ffef337055026c234c76b9faa.MainApplication.__md_methods);
+		
+	}
+}
---
+++ MyMap.Android/obj/Debug/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 43e1dd207d832eb55e97898e43a2b0f9c6e3e24c */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1592
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 417105
+	.include	"typemap.jm.inc"
